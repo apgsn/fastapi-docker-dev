f1845168d76b91d9886b6dc0c9d3023f4ddf78de
apgsn <apgsn@yahoo.com>
2022-02-03 22:00:15 +0100

    Set up CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 
 # Creats table in db if it doesn't exist (obsolete, use alembic instead)
 # Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(user.router)
